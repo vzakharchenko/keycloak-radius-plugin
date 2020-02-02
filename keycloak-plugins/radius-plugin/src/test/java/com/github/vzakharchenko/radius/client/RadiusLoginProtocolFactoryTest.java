8ca1f01f2f63273e69c1407034076e2924b9a57c
vzakharchenko <vzakharchenko@provectus.com>
2020-02-02 15:36:42 +0200

    OneTime Radius Password

--- keycloak-plugins/radius-plugin/src/test/java/com/github/vzakharchenko/radius/client/RadiusLoginProtocolFactoryTest.java
+++ keycloak-plugins/radius-plugin/src/test/java/com/github/vzakharchenko/radius/client/RadiusLoginProtocolFactoryTest.java
@@ -1,9 +1,17 @@
 package com.github.vzakharchenko.radius.client;
 
+import com.github.vzakharchenko.radius.test.AbstractRadiusTest;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.protocol.LoginProtocol;
+import org.keycloak.protocol.LoginProtocolFactory;
+import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import com.github.vzakharchenko.radius.test.AbstractRadiusTest;
 
+import java.util.HashMap;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.*;
 
 public class RadiusLoginProtocolFactoryTest extends AbstractRadiusTest {
@@ -15,12 +23,24 @@ public class RadiusLoginProtocolFactoryTest extends AbstractRadiusTest {
         radiusLoginProtocolFactory.createDefaultClientScopes(null, false);
         radiusLoginProtocolFactory.close();
         radiusLoginProtocolFactory.init(null);
-        radiusLoginProtocolFactory.postInit(keycloakSessionFactory);
+
         assertNull(radiusLoginProtocolFactory.createProtocolEndpoint(null, null));
         assertNotNull(radiusLoginProtocolFactory.create(session));
         assertEquals(radiusLoginProtocolFactory.getBuiltinMappers().size(), 0);
         Assert.assertEquals(radiusLoginProtocolFactory.getId(), RadiusLoginProtocolFactory.RADIUS_PROTOCOL);
 
+
+    }
+
+    @Test
+    public void testPostInit() {
+        LoginProtocolFactory protocolFactory = mock(LoginProtocolFactory.class);
+        when(keycloakSessionFactory.getProviderFactory(LoginProtocol.class, OIDCLoginProtocol.LOGIN_PROTOCOL))
+                .thenReturn(protocolFactory);
+        HashMap<String, ProtocolMapperModel> map = new HashMap<>();
+        when(protocolFactory.getBuiltinMappers()).thenReturn(map);
+        radiusLoginProtocolFactory.postInit(keycloakSessionFactory);
+        assertEquals(map.size(),1);
 
     }
 }
--- keycloak-plugins/radius-plugin/src/test/java/com/github/vzakharchenko/radius/radius/handlers/session/KeycloakSessionUtilsTest.java
+++ keycloak-plugins/radius-plugin/src/test/java/com/github/vzakharchenko/radius/radius/handlers/session/KeycloakSessionUtilsTest.java
@@ -3,9 +3,10 @@ package com.github.vzakharchenko.radius.radius.handlers.session;
 import com.github.vzakharchenko.radius.test.AbstractRadiusTest;
 import org.testng.annotations.Test;
 
+import static com.github.vzakharchenko.radius.mappers.RadiusPasswordMapper.RADIUS_SESSION_PASSWORD;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
 
 public class KeycloakSessionUtilsTest extends AbstractRadiusTest {
@@ -15,10 +16,25 @@ public class KeycloakSessionUtilsTest extends AbstractRadiusTest {
                 .thenReturn(null);
         assertFalse(KeycloakSessionUtils.isActiveSession(session, "test", REALM_RADIUS_NAME));
     }
-        @Test
+
+    @Test
     public void getRadiusInfo() {
         assertNotNull(KeycloakSessionUtils.getRadiusUserInfo(session));
         assertNotNull(KeycloakSessionUtils.getRadiusSessionInfo(session));
         assertTrue(KeycloakSessionUtils.isActiveSession(session, "test", REALM_RADIUS_NAME));
+    }
+
+    @Test
+    public void getClearOneTimePassword() {
+        when(radiusUserInfo.getActivePassword()).thenReturn("123");
+        KeycloakSessionUtils.clearOneTimePassword(session);
+        verify(userSessionModel).removeNote(RADIUS_SESSION_PASSWORD);
+    }
+
+    @Test
+    public void getClearOneTimePasswordNotDeleted() {
+        when(radiusUserInfo.getActivePassword()).thenReturn("wrong");
+        KeycloakSessionUtils.clearOneTimePassword(session);
+        verify(userSessionModel, never()).removeNote(RADIUS_SESSION_PASSWORD);
     }
 }
