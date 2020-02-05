9767d0b5784e19f5ec9e5805215281bfe075192d
vzakharchenko <vzakharchenko@provectus.com>
2020-02-05 23:12:43 +0200

    updated version

--- keycloak/init.sh
+++ keycloak/init.sh
@@ -1,2 +1,2 @@
 rm -f keycloak_source.zip
-wget https://downloads.jboss.org/keycloak/8.0.1/keycloak-8.0.1.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
+wget https://downloads.jboss.org/keycloak/8.0.2/keycloak-8.0.2.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
