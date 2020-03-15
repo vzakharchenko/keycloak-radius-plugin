11ee18390f2846dc931a077196e32a425a97810d
vzakharchenko <vzakharchenko@provectus.com>
2020-03-15 13:40:21 +0200

    removed hotspot example

--- hotspot/source/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  setupFiles: ['./unitTestConfig/jestSetup.js'],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-};
