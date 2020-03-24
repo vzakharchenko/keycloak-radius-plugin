95aa8b35a31d0017517d3eec4241b593548e8a22
vzakharchenko <vzakharchenko@provectus.com>
2020-03-24 15:28:53 +0200

    fix

--- keycloak-plugins/radius-plugin/src/main/java/com/github/vzakharchenko/radius/password/RadiusCredentialProvider.java
+++ keycloak-plugins/radius-plugin/src/main/java/com/github/vzakharchenko/radius/password/RadiusCredentialProvider.java
@@ -68,8 +68,8 @@ public class RadiusCredentialProvider implements
     }
 
     @Override
-    public void deleteCredential(RealmModel realm, UserModel user, String credentialId) {
-        getCredentialStore().removeStoredCredential(realm, user, credentialId);
+    public boolean deleteCredential(RealmModel realm, UserModel user, String credentialId) {
+        return getCredentialStore().removeStoredCredential(realm, user, credentialId);
     }
 
     @Override
