3662b55c7b7654650722388b7de8e0562d35c470
vzakharchenko <vzakharchenko@provectus.com>
2020-04-29 09:13:10 +0300

    update scripts for keycloak 9.0.3

--- keycloak/init.sh
+++ keycloak/init.sh
@@ -1,2 +1,2 @@
 rm -f keycloak_source.zip
-wget https://downloads.jboss.org/keycloak/9.0.2/keycloak-9.0.2.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
+wget https://downloads.jboss.org/keycloak/9.0.3/keycloak-9.0.3.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
