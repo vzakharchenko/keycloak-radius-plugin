860b6bfdc0fe025814dbed38c9ffe43abed742be
vzakharchenko <vzakharchenko@modeln.com>
2020-07-23 09:55:15 +0300

    updated keycloak version

--- keycloak-plugins/radius-plugin/src/main/java/com/github/vzakharchenko/radius/radius/handlers/attributes/AuthorizationAttributes.java
+++ keycloak-plugins/radius-plugin/src/main/java/com/github/vzakharchenko/radius/radius/handlers/attributes/AuthorizationAttributes.java
@@ -11,6 +11,7 @@ import org.keycloak.authorization.model.ResourceServer;
 import org.keycloak.authorization.permission.ResourcePermission;
 import org.keycloak.authorization.permission.evaluator.PermissionEvaluator;
 import org.keycloak.authorization.policy.evaluation.Result;
+import org.keycloak.authorization.store.StoreFactory;
 import org.keycloak.models.ClientModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
@@ -35,11 +36,14 @@ public class AuthorizationAttributes extends AbstractKeycloakAttributes<Resource
     protected Set<Resource> getKeycloakTypes(AuthorizationProvider authorizationProvider,
                                              ResourceServer resourceServer,
                                              UserModel userModel, RealmModel realmModel) {
-        List<Resource> resources = authorizationProvider.getStoreFactory()
+        StoreFactory storeFactory = authorizationProvider.getStoreFactory();
+        List<Resource> resources = storeFactory
                 .getResourceStore().findByResourceServer(resourceServer.getId());
         PermissionEvaluator permissionEvaluator = authorizationProvider.evaluators()
                 .from(resources.stream().map(resource ->
-                                new ResourcePermission(resource, resource.getResourceServer(),
+                                new ResourcePermission(resource, storeFactory
+                                        .getResourceServerStore()
+                                        .findById(resource.getResourceServer()),
                                         new HashMap<>())).collect(Collectors.toList()),
                         new DefaultEvaluationContext(
                                 new UserModelIdentity(realmModel, userModel), session));
