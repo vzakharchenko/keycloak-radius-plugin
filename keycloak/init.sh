a303e70e9f63f8e1aaf53d6801faf4a0292a2d87
vzakharchenko <vzakharchenko@modeln.com>
2020-09-09 13:36:34 +0300

    update keycloak version

--- keycloak/init.sh
+++ keycloak/init.sh
@@ -1,2 +1,2 @@
 rm -f keycloak_source.zip
-wget https://downloads.jboss.org/keycloak/11.0.1/keycloak-11.0.1.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
+wget https://downloads.jboss.org/keycloak/11.0.2/keycloak-11.0.2.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
