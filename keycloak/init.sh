8db20c40943d0f32f6be3fe85a7941d978bf7a1d
vzakharchenko <vzakharchenko@modeln.com>
2020-12-17 10:48:23 +0200

    Keycloak 12.0.0

--- keycloak/init.sh
+++ keycloak/init.sh
@@ -1,2 +1,2 @@
 rm -f keycloak_source.zip
-wget https://downloads.jboss.org/keycloak/12.0.0/keycloak-12.0.0.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
+wget https://github.com/keycloak/keycloak/releases/download/12.0.0/keycloak-12.0.0.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
