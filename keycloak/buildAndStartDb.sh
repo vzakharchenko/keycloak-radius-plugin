a158a57246a3f42758f803d609c3eb404029a141
vzakharchenko <vzakharchenko@modeln.com>
2021-06-19 17:52:12 +0300

    updated version

--- keycloak/buildAndStartDb.sh
+++ keycloak/buildAndStartDb.sh
@@ -1,5 +1,5 @@
 set -e
 ./build.sh
-rm -rf target/keycloak/keycloak-13.0.1/standalone/data
-cp -r data target/keycloak/keycloak-13.0.1/standalone/data
+rm -rf target/keycloak/keycloak-14.0.0/standalone/data
+cp -r data target/keycloak/keycloak-14.0.0/standalone/data
 ./start.sh
--- keycloak/init.sh
+++ keycloak/init.sh
@@ -1,2 +1,2 @@
 rm -f keycloak_source.zip
-wget https://github.com/keycloak/keycloak/releases/download/13.0.1/keycloak-13.0.1.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
+wget https://github.com/keycloak/keycloak/releases/download/14.0.0/keycloak-14.0.0.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
