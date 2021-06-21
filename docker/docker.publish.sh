0864caa3067dff85e107797e02e16f5c6fd5be30
vzakharchenko <vzakharchenko@modeln.com>
2021-06-21 07:45:54 +0300

    updated version of docker image to 1.3.8

--- docker/docker.publish.sh
+++ docker/docker.publish.sh
@@ -2,8 +2,8 @@ rm -rf ./cli
 cp -r ../cli ./cli
 
 docker build -t keycloak-radius-plugin .
-docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:1.3.7
-docker push vassio/keycloak-radius-plugin:1.3.7
+docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:1.3.8
+docker push vassio/keycloak-radius-plugin:1.3.8
 
 docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:latest
 docker push vassio/keycloak-radius-plugin:latest
