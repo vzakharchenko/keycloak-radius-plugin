cd7931a90daed5abb3530ca2f4ce83c592eae307
vzakharchenko <vzakharchenko@modeln.com>
2021-12-08 10:06:00 +0200

    update docker

--- docker/docker.publish.sh
+++ docker/docker.publish.sh
@@ -2,8 +2,8 @@ rm -rf ./cli
 cp -r ../cli ./cli
 
 docker build -t keycloak-radius-plugin .
-docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:1.3.13
-docker push vassio/keycloak-radius-plugin:1.3.13
+docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:1.3.14
+docker push vassio/keycloak-radius-plugin:1.3.14
 
 docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:latest
 docker push vassio/keycloak-radius-plugin:latest
