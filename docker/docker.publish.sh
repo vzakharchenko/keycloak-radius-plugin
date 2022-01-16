5d61f65febdec3ff17d04bf80f7ba56a0525320a
vzakharchenko <vzakharchenko@modeln.com>
2022-01-16 11:46:56 +0200

    remoced log4j

--- docker/docker.publish.sh
+++ docker/docker.publish.sh
@@ -2,8 +2,8 @@ rm -rf ./cli
 cp -r ../cli ./cli
 
 docker build -t keycloak-radius-plugin .
-docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:1.3.17
-docker push vassio/keycloak-radius-plugin:1.3.17
+docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:1.3.18
+docker push vassio/keycloak-radius-plugin:1.3.18
 
 docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:latest
 docker push vassio/keycloak-radius-plugin:latest
