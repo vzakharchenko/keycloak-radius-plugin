e12a715063641076da72e795ae03b93b7b17c694
vzakharchenko <vzakharchenko@modeln.com>
2022-02-02 09:05:05 +0200

    update keycloak version

--- docker/docker.publish.sh
+++ docker/docker.publish.sh
@@ -2,8 +2,8 @@ rm -rf ./cli
 cp -r ../cli ./cli
 
 docker build -t keycloak-radius-plugin .
-docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:1.3.18
-docker push vassio/keycloak-radius-plugin:1.3.18
+docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:1.3.19
+docker push vassio/keycloak-radius-plugin:1.3.19
 
 docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:latest
 docker push vassio/keycloak-radius-plugin:latest
--- keycloak/saveDb.sh
+++ keycloak/saveDb.sh
@@ -1,3 +1,3 @@
  set -e
  rm -rf data
- cp -r target/keycloak/keycloak-16.1.0/standalone/data data
+ cp -r target/keycloak/keycloak-16.1.1/standalone/data data
