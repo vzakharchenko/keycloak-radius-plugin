abe581cc592b41052d30d9a76d7c28e290c70513
vzakharchenko <vzakharchenko@modeln.com>
2022-06-20 16:24:35 +0300

    pre release

--- keycloak-quarkus/init.sh
+++ keycloak-quarkus/init.sh
@@ -1,2 +1,2 @@
 rm -f keycloak_source.zip
-wget https://github.com/keycloak/keycloak/releases/download/18.0.0/keycloak-18.0.0.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
+wget https://github.com/keycloak/keycloak/releases/download/18.0.1/keycloak-18.0.1.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
--- keycloak-quarkus/saveDb.sh
+++ keycloak-quarkus/saveDb.sh
@@ -1,3 +1,3 @@
  set -e
  rm -rf data
- cp -r target/keycloak/keycloak-18.0.0/data data
+ cp -r target/keycloak/keycloak-18.0.1/data data
