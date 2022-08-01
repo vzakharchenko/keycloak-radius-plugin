862988585f5276a4f4bd9481b80eaaea682351eb
vzakharchenko <vzakharchenko>
2022-08-01 20:08:31 +0300

    keycloak 19.0.1

--- docker/docker.publish.sh
+++ docker/docker.publish.sh
@@ -1,8 +1,8 @@
 rm -rf ./cli
 
 docker build -t keycloak-radius-plugin .
-docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:1.4.6
-docker push vassio/keycloak-radius-plugin:1.4.6
+docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:1.4.7
+docker push vassio/keycloak-radius-plugin:1.4.7
 
 docker tag  keycloak-radius-plugin vassio/keycloak-radius-plugin:latest
 docker push vassio/keycloak-radius-plugin:latest
