3162385596ccdf6d42ac7926a66db8d77a096fd7
vzakharchenko <vzakharchenko>
2022-08-01 19:33:06 +0300

    fixed build

--- keycloak-plugins/radius-disconnect-plugin/src/main/java/com/github/vzakharchenko/radius/dm/logout/RadiusClusterAwareScheduledTaskRunner.java
+++ keycloak-plugins/radius-disconnect-plugin/src/main/java/com/github/vzakharchenko/radius/dm/logout/RadiusClusterAwareScheduledTaskRunner.java
@@ -5,33 +5,39 @@ import org.keycloak.cluster.ClusterProvider;
 import org.keycloak.cluster.ExecutionResult;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.services.scheduled.ClusterAwareScheduledTaskRunner;
 import org.keycloak.services.scheduled.ScheduledTaskRunner;
 import org.keycloak.timer.ScheduledTask;
 
 @Deprecated
 public class RadiusClusterAwareScheduledTaskRunner extends ScheduledTaskRunner {
-    private static final Logger logger = Logger.getLogger(org.keycloak.services.scheduled.ClusterAwareScheduledTaskRunner.class);
+    private static final Logger LOGGER = Logger
+            .getLogger(ClusterAwareScheduledTaskRunner.class);
     private final int intervalSecs;
 
-    public RadiusClusterAwareScheduledTaskRunner(KeycloakSessionFactory sessionFactory, ScheduledTask task, long intervalMillis) {
+    public RadiusClusterAwareScheduledTaskRunner(KeycloakSessionFactory sessionFactory,
+                                                 ScheduledTask task, long intervalMillis) {
         super(sessionFactory, task);
         this.intervalSecs = (int) (intervalMillis / 1000L);
     }
 
+    @Override
     protected void runTask(final KeycloakSession session) {
         session.getTransactionManager().begin();
         ClusterProvider clusterProvider = session.getProvider(ClusterProvider.class);
         ScheduledTask scheduledTask = this.task;
         String taskKey = scheduledTask.getClass().getSimpleName();
-        ExecutionResult<Void> result = clusterProvider.executeIfNotExecuted(taskKey, this.intervalSecs, () -> {
-            scheduledTask.run(session);
-            return null;
-        });
+        ExecutionResult<Void> result = clusterProvider
+                .executeIfNotExecuted(taskKey, this.intervalSecs, () -> {
+                    scheduledTask.run(session);
+                    return null;
+                });
         session.getTransactionManager().commit();
         if (result.isExecuted()) {
-            logger.debugf("Executed scheduled task %s", taskKey);
+            LOGGER.debugf("Executed scheduled task %s", taskKey);
         } else {
-            logger.debugf("Skipped execution of task %s as other cluster node is executing it", taskKey);
+            LOGGER.debugf("Skipped execution of task %s as other cluster node is executing it",
+                    taskKey);
         }
 
     }
