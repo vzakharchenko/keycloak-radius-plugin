089a051e09962fbf0c7d4ae70f8e1b956f66efc9
elkman <elkman@users.noreply.github.com>
2023-09-15 17:59:34 +0200

    bump Keycloak version from 22.0.1 to 22.0.3
    fixes CVE-2023-4918

--- keycloak-quarkus/buildAndStartDb.sh
+++ keycloak-quarkus/buildAndStartDb.sh
@@ -1,5 +1,5 @@
 set -e
 ./build.sh
-rm -rf target/keycloak/keycloak-22.0.1/data
-cp -r data target/keycloak/keycloak-22.0.1/data
+rm -rf target/keycloak/keycloak-22.0.3/data
+cp -r data target/keycloak/keycloak-22.0.3/data
 ./start.sh
