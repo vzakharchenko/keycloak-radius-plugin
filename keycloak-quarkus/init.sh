a203fcf737653215b647a75129a09b13756e4398
Charley Wu <akunzai@gmail.com>
2024-02-01 16:33:53 +0800

    Bump Keycloak version from 21.0.0 to 23.0.5

--- keycloak-quarkus/init.sh
+++ keycloak-quarkus/init.sh
@@ -1,2 +1,2 @@
 rm -f keycloak_source.zip
-wget https://github.com/keycloak/keycloak/releases/download/21.0.0/keycloak-21.0.0.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
+wget https://github.com/keycloak/keycloak/releases/download/23.0.5/keycloak-23.0.5.zip -O keycloak_source.zip  -q --show-progress --progress=bar:force 2>&1
--- keycloak-quarkus/start.sh
+++ keycloak-quarkus/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-cd target/keycloak/keycloak-21.0.0
+cd target/keycloak/keycloak-23.0.5
 bin/kc.sh --debug 8190 start-dev --http-port=8090
